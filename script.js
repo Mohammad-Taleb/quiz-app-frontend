bb2270b478dd30dd5fdd3d1cbd0045e4488437cd
Mohammad-Taleb <akamohdtaleb@gmail.com>
2025-04-21 13:39:47 +0300

    Add function <> adding the showtab function that allow us to switch between register&login tabs

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function showTab(tabId){
+  const allTabs = document.querySelectorAll('.tab');
+  allTabs.forEach(tab => tab.classList.remove('active'));
+
+  const activeTab = document.getElementById(tabId);
+  activeTab.classList.add('active');
+}
+
+
+
+
+
 function register(){
   const email = document.getElementById("regEmail").value;
   const password = document.getElementById("regPassword").value;
